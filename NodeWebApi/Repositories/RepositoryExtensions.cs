d92ea38c9911a9c3b1135f2bd6f6da3fb8b96140
niekschoemaker <32094562+niekschoemaker@users.noreply.github.com>
2021-12-02 19:33:08 +0100

    NodeWebApi: Add RepositoryExtensions to add all needed services for the repositories

---
+++ NodeWebApi/Repositories/RepositoryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using NodeWebApi.Repositories.Transactions;
+using NodeWebApi.Repositories.Wallets;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class RepositoryExtensions
+{
+    public static IServiceCollection AddRepositories(
+        this IServiceCollection services)
+    {
+        services.AddScoped<ITransactionsRepository, TransactionsRepository>();
+        services.AddScoped<IWalletsRepository, WalletsRepository>();
+
+        return services;
+    }
+}
+
