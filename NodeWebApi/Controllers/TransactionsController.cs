f88ca4e245ba5994ea2de1b859eb8ee4eecc8232
Ivo Gerner <i.gerner@outlook.com>
2021-12-16 10:32:44 +0100

    Cleaned up commented code

--- NodeWebApi/Controllers/TransactionsController.cs
+++ NodeWebApi/Controllers/TransactionsController.cs
@@ -59,43 +59,15 @@ namespace NodeWebApi.Controllers
 
 
             // Opmaak van data benodigd voor het signen
-            // transaction amount en de transaction output worden in 1 byte array gezet; transaction output is de public key van de ontvanger
             byte[] data = repository.SignatureDataConvertToBytes(transaction);
-
-            //// Sign data
-            //// transaction input is de public key van de verzender
-            //using (ECDsaCng dsa = new ECDsaCng(CngKey.Create(CngAlgorithm.ECDsaP256)))
-            //{
-            //    dsa.HashAlgorithm = CngAlgorithm.Sha256;
-            //    transaction.Input = dsa.Key.Export(CngKeyBlobFormat.EccPublicBlob);
-            //    transaction.Signature = dsa.SignData(data);
-            //}
 
             // Controleren of signature overeenkomt
             // Public key van verzender wordt gebruikt om te controleren of de data en de signature te verifieren
-
-           
-            //using (ECDsaCng ecsdKey = new ECDsaCng(CngKey.Import(transaction.Input, CngKeyBlobFormat.EccPublicBlob)))
-            //{
-            //    Console.Write(this.GetType().Name);
-            //    if (ecsdKey.VerifyData(data, transaction.Signature))
-            //        Console.WriteLine(" data is good");
-            //    else
-            //        Console.WriteLine(" data is bad");
-            //}
-
             ECDsaKey ecdsKey = new ECDsaKey(transaction.Input, false);
-            Console.Write(this.GetType().Name);
             if (ecdsKey.Verify(data, transaction.Signature))
-            {
-                Console.WriteLine(" data is good");
                 repository.CreateTransaction(transaction);
-            }
             else
-            {
-                Console.WriteLine(" data is bad");
                 return BadRequest();
-            }
 
             return CreatedAtAction(nameof(GetTransaction), new { id = transaction.Id }, transaction.AsDto());
         }
